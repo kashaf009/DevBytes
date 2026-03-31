759850700c22f2b17b200770a8332fbc7afc426f
kashaf khan <mdkashaf9931@gmail.com>
2026-03-31 20:19:44 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,16 @@ import express from "express";
 
 const App = express();
 
-App.use((req,res)=>{
+App.use( "/test/4" ,(req,res)=>{
+    res.send("hello from the test 4 server")
+} )
+
+App.use( "/test" ,(req,res)=>{
+    res.send("hello from the test server")
+} )
+
+
+App.use( "/" ,(req,res)=>{
     res.send("hello from the server")
 } )
 

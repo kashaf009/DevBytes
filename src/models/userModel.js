e524acd9165682dca6be5a15c1e46f9cb468a6e8
kashaf khan <mdkashaf9931@gmail.com>
2026-04-04 21:13:54 +0530

    updated and optimized user schema

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,22 +3,37 @@ import mongoose from "mongoose";
 
 const userSchema= new mongoose.Schema({
     firstName: {
-        type:String
+        type:String,
+        required:true
     },
     lastName: {
         type:String
     },
     emailId: {
-        type:String
+        type:String,
+        required:true,
+        lowercase:true
     },  
     password: {
-        type:String
+        type:String,
+        required:true
     },
     age: {
         type:Number
     },
     gender:{
         type:String
+    },
+    photoUrl:{
+        type:String,
+        default:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQVE7sMtLdrFVWAef8WEWzR9TzkKPCj9Gd1xpavbjYKkvQADVy4&s"
+    },
+    skills:{
+        type:String,
+
+    },
+    about:{
+        type:String,
     }
 
 })

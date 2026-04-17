1f716faf48dbc5a679be5f3d52e120c402df4985
kashaf khan <mdkashaf9931@gmail.com>
2026-04-17 16:13:39 +0530

    made get  feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,8 @@ import express from "express";
 
 import { userAuth } from "../middleware/auth.js";
 import connectionRequest from "../models/connectionRequest.js";
+import user from "../models/userModel.js";
+import { set } from "mongoose";
 
 const userRoutes = express.Router();
 
@@ -54,9 +56,9 @@ userRoutes.get("/user/connections", userAuth, async (req, res) => {
     const data = connectionRequestData.map((data) => {
       if (data.fromUserId._id.equals(currentUser._id)) {
         return data.toUserId;
-      }else{
-      return data.fromUserId;
-    }
+      } else {
+        return data.fromUserId;
+      }
     });
 
     res.json({ connection: data });
@@ -64,6 +66,47 @@ userRoutes.get("/user/connections", userAuth, async (req, res) => {
     res.status(404).json({
       message: "Error" + error.message,
     });
+  }
+});
+
+// get feed
+userRoutes.get("/feed", userAuth, async (req, res) => {
+  try {
+    // Don't show connections, ignore, accepted in the feed.
+
+    const loginUser = req.user;
+
+    const connectionRequestData = await connectionRequest
+      .find({
+        $or: [{ fromUserId: loginUser._id }, { toUserId: loginUser._id }],
+      })
+      .select("fromUserId  toUserId");
+
+    
+
+    const uniqueIds = new Set();
+
+
+    connectionRequestData.forEach((data) => {
+      uniqueIds.add(data.fromUserId._id.toString());
+      uniqueIds.add(data.toUserId._id.toString());
+    });
+
+    console.log(uniqueIds);
+    
+
+    const hideProfileData = await user.find({
+      $and: [
+        { _id: { $nin: Array.from(uniqueIds) } },
+        { _id: { $ne: loginUser._id } },
+      ],
+    }).select(SAFE_USER_DATA);
+
+    res.json({ feed: hideProfileData });
+
+
+  } catch (error) {
+    res.status(404).json({ message: "Error" + error.message });
   }
 });
 

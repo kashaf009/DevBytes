136194422f378fcc486c0f96a3881ba64fe7e98c
kashaf khan <mdkashaf9931@gmail.com>
2026-05-06 17:04:53 +0530

    build chat feature

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -8,9 +8,22 @@ const initilizeSocket = (server) => {
   });
 
   io.on("connection", (socket) => {
-    socket.on("joinChat", (data) => {}   );
+    socket.on("joinChat", ({firstName, loginUserId, targetUserId}) => {
+      const roomId = [loginUserId, targetUserId].sort().join("_");
+      console.log( firstName + " joined : " +roomId);
+      
+      socket.join(roomId);
+    });
 
-    socket.on("sendMessage", (data) => {});
+    socket.on("sendMessage", ({firstName,loginUserId,photoUrl,targetUserId,text}) => {
+      const roomId = [loginUserId, targetUserId].sort().join("_");
+      // console.log(firstName +" : "+ text);
+      
+
+      io.to(roomId).emit("messageRecieved",{firstName,text,photoUrl})
+
+
+    });
 
     socket.on("disconnect", () => {});
   });
